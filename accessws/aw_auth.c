3f91bda89bea3df92f56e3d18152e5ee7b5e9979
ajanva <bekabadski@gmail.com>
2025-02-09 16:32:02 +0700

    b

--- accessws/aw_auth.c
+++ accessws/aw_auth.c
@@ -65,9 +65,11 @@ cleanup:
 
 static void on_result(struct state_data *state, sds token, json_t *result)
 {
+    /*
     if (state->ses->id != state->ses_id)
         log_error("SESSION ID NOT SAME");
         return;
+    */
     if (result == NULL)
         log_error("no result from frontend server");
         goto error;
